543e797090626979e2d49820059019edefa11ab4
Dmitry Karpenkov <Dimonwars@mail.ru>
2025-02-08 19:34:30 +0300

    Добавьте файлы проекта.

---
+++ EnterWordProgramm/DictionaryAPI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterWordProgramm
+{
+    internal class DictionaryAPI
+    {
+        public static async Task EntriesAPI(string word)
+        {
+            string apiUrl = $"https://api.dictionaryapi.dev/api/v2/entries/en/{word}";
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonResponse = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine($"JSON Response:\n${jsonResponse}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: {response.StatusCode}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Exception: {ex.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ EnterWordProgramm/Program.cs
@@ -0,0 +1,11 @@
+﻿using EnterWordProgramm;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Введите английское слово для поиска");
+        string userWrite = Console.ReadLine();
+        await DictionaryAPI.EntriesAPI(userWrite.ToLower());
+    }
+}
